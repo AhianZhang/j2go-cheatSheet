1cdb590a13cd6869c758046c3d925014c8e415e3
ahianzhang <augerta1995@gmail.com>
2022-06-27 17:28:47 +0800

    update

--- golang/syntax/basic.go
+++ golang/syntax/basic.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"reflect"
+	"unicode"
 )
 
 func main() {
@@ -57,13 +58,18 @@ func main() {
 	// rune alias int32
 	str := "abc"
 	for _, v := range str {
-		fmt.Println(v, reflect.TypeOf(v))
+		fmt.Println(v, reflect.TypeOf(v)) //rune
 	}
 	for i := range str {
-		fmt.Println(str[i], reflect.TypeOf(str[i]))
+		fmt.Println(str[i], reflect.TypeOf(str[i])) //byte
 	}
 	r := []rune(str)
 	for _, v := range r {
 		fmt.Println(v, reflect.TypeOf(v))
 	}
+	fmt.Println("IsLetter", unicode.IsLetter('a'))
+	fmt.Println("IsDigit", unicode.IsDigit('1'))
+	fmt.Println("IsLower", unicode.IsLower('A'))
+	fmt.Println("IsNumber", unicode.IsNumber('1'))
+	fmt.Println(reflect.TypeOf("1123"[1])) // byte
 }
