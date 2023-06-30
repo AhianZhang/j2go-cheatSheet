5a4c3ae4fcba381d60a929817ba8b90bf5b634fd
ahianzhang <augerta1995@gmail.com>
2023-06-30 23:09:13 +0800

    feat: add generation

--- java/syntax/Main.java
+++ java/syntax/Main.java
@@ -4,5 +4,12 @@ public class Main{
     public static void main(String[] args) {
      // basic data type
      int num = 1;
+     boolean flag = true;
+        char ch = 'a';
+        double d = 1.0;
+        float f = 1.0f;
+        long l = 1L;
+        short s = 1;
+
     }    
 }
